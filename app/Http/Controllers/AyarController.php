937280ab1ee94a913c13df2f788fad90137a2d05
jafarnuri <jafarnuri1994@gmail.com>
2023-10-25 12:06:38 +0400

    .

--- app/Http/Controllers/AyarController.php
+++ app/Http/Controllers/AyarController.php
@@ -4,20 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Ayarlar;
+use Illuminate\Support\Facades\File;
 
 class AyarController extends Controller
 {
  public function genelayarelaveet(Request $request){
 
-    $request->validate([
-        'ayar_bashliq'=>'required',
-        'ayar_achiqlama'=>'required',
-        'ayar_kilidsoz'=>'required',
-        'ayar_yazar'=>'required'
 
-    ]);
-
-    $ayarlar=new Ayarlar();
+    $ayarlar= new Ayarlar();
     $ayarlar->ayar_bashliq=$request->ayar_bashliq;
     $ayarlar->ayar_achiqlama=$request->ayar_achiqlama;
     $ayarlar->ayar_kilidsoz=$request->ayar_kilidsoz;
--- app/Models/Ayarlar.php
+++ app/Models/Ayarlar.php
@@ -10,7 +10,7 @@ class Ayarlar extends Model
     use HasFactory;
     protected $table="ayarlar";
     protected $fillabe=["id",
-    "ayar_bashliq","ayar_resim","ayar_logo","ayar_achiqlama","ayar_kilidsoz","ayar_yazar",
+    "ayar_bashliq",'ayar_resim',"ayar_logo","ayar_achiqlama","ayar_kilidsoz","ayar_yazar",
     "ayar_tel","ayar_map","ayar_faks","ayar_mail","ayar_olke",
     "ayar_sheher","ayar_adres","ayar_mesai","ayar_facebook","ayar_twitter",
     "ayar_google","ayar_youtube","created_at","updated_at"];
